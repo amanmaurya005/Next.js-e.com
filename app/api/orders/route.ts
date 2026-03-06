afaf9400581a46f639523092df703f11463225c2
amanmaurya005 <amaurya9465@gmail.com>
2026-03-06 17:22:30 +0530

    error fix

---
+++ app/api/orders/route.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server"
+
+export async function GET() {
+  return NextResponse.json({ message: "Orders API working" })
+}
+
+export async function POST(req: Request) {
+  const body = await req.json()
+
+  return NextResponse.json({
+    message: "Order created",
+    data: body
+  })
+}
