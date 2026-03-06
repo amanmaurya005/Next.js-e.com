730094fa63126289b6361053613a2af27ee1f8bb
amanmaurya005 <amaurya9465@gmail.com>
2026-03-06 17:01:58 +0530

    vercel error solve

--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,50 @@
-// import React from 'react'
+import React from 'react'
 
-// export default function page() {
-//   return (
-//     <>
-//     <header className='flex items-center justify-between px-8 bg-amber-600 text-white'>
-//       <div className='w-1/3'>logo</div>
-//       <nav className='w-4/6'>
-//         <ul className='flex items-center  justify-evenly h-18'>
-//           <li>home</li>
-//           <li>about</li>
-//           <li className='text-amber-900'>blog</li>
-//           <li className='text-amber-300'>contact</li>
-//         </ul>
-//       </nav>
-//     </header>
-//     </>
-//   )
-// }
-
-"use client"
-
-import { useEffect, useState } from "react"
-
-export default function Products() {
-
-  const [products,setProducts] = useState([])
-
-  useEffect(()=>{
-    fetch("/api/products")
-      .then(res => res.json())
-      .then(data => setProducts(data))
-  },[])
-
+export default function page() {
   return (
-    <div>
-
-      <h1>Products</h1>
-
-      {products.map((p:any)=>(
-        <div key={p._id}>
-          <h3>{p.title}</h3>
-          <p>{p.price}</p>
-          <img src={p.image} alt={p.title} />
-        </div>
-      ))}
-
-    </div>
+    <>
+    <header className='flex items-center justify-between px-8 bg-amber-600 text-white'>
+      <div className='w-1/3'>logo</div>
+      <nav className='w-4/6'>
+        <ul className='flex items-center  justify-evenly h-18'>
+          <li>home</li>
+          <li>about</li>
+          <li className='text-amber-900'>blog</li>
+          <li className='text-amber-300'>contact</li>
+        </ul>
+      </nav>
+    </header>
+    </>
   )
 }
+
+// "use client"
+
+// import { useEffect, useState } from "react"
+
+// export default function Products() {
+
+//   const [products,setProducts] = useState([])
+
+//   useEffect(()=>{
+//     fetch("/api/products")
+//       .then(res => res.json())
+//       .then(data => setProducts(data))
+//   },[])
+
+//   return (
+//     <div>
+
+//       <h1>Products</h1>
+
+//       {products.map((p:any)=>(
+//         <div key={p._id}>
+//           <h3>{p.title}</h3>
+//           <p>{p.price}</p>
+//           <img src={p.image} alt={p.title} />
+//         </div>
+//       ))}
+
+//     </div>
+//   )
+// }
